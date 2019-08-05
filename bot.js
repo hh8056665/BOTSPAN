d09cc5d54b150ee126623ad55219a5499c7f0a88
hh8056665 <53127661+hh8056665@users.noreply.github.com>
2019-08-05 14:00:28 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("465078872266768384")
+setInterval(function() {
+channel.send(`hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906 hassam1906`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
